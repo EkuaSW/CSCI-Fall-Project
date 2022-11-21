45259a1a8ea9b6ffd2702e41bb1c06c508f30256
Dorcas <114271658+EkuaSW@users.noreply.github.com>
2022-11-21 13:31:20 -0600

    Add-games-data-project

---
+++ Games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO sandbox(int32_value, bool_value, double_value) VALUES (8765345, 0, 2359845.32);
+
+--UPDATE sandbox SET int64_value = 4839575 WHERE id = 19;
+
+--DELETE FROM sandbox WHERE id = 28;
